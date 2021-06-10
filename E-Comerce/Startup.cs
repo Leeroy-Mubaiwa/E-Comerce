93e332085f986ed8cbee54ea0cbbebcc9b605a43
Leeroy-Mubaiwa <markmubaiwa@gmail.com>
2021-06-10 15:11:15 +0200

    Add project files.

---
+++ E-Comerce/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Comerce.Startup))]
+namespace E_Comerce
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
